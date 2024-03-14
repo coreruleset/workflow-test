c9f246bb27d112b390a6fb3f1fd1398706e86f93
Max Leske <maxleske@gmail.com>
2024-03-14 21:44:26 +0100

    chore: check codeql

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/coreruleset/workflow-test/v2
+
+go 1.22.1
---
+++ main.go
@@ -0,0 +1,10 @@
+// Copyright 2023 OWASP ModSecurity Core Rule Set Project
+// SPDX-License-Identifier: Apache-2.0
+
+// Package go-ftw is a Framework for Testing Web Application Firewalls
+// It is derived from the work made with the pytest plugin `ftw`
+package main
+
+func main() {
+	println("Hello world")
+}
